008b2a5ac8544db710127e149160f9ca1db7ec4c
nainteeth <knusperpommes@gmail.com>
2026-01-22 20:01:23 +0100

    Undoing stupid stuff and actually fixing compline

--- home/dotfiles/doom/config.el
+++ home/dotfiles/doom/config.el
@@ -32,10 +32,10 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-compline)
+(setq doom-theme 'compline)
 
 ;; Enable the menu bar
-(menu-bar-mode 1)
+;; (menu-bar-mode 1)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
--- home/dotfiles/doom/custom.el
+++ home/dotfiles/doom/custom.el
@@ -5,7 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3613617b9953c22fe46ef2b593a2e5bc79ef3cc88770602e7e569bbd71de113b"
+   '("e63807a0b0bdb8dc1b0b42ef44e92951305a0053f504eb1a4aee3dfcd5ea4946"
+     "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a"
+     "3613617b9953c22fe46ef2b593a2e5bc79ef3cc88770602e7e569bbd71de113b"
      "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd"
      "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default)))
 (custom-set-faces
