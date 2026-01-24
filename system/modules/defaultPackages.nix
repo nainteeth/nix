3f0210d59b306a344d6fd40efde8a48ed875eec7
nainteeth <knusperpommes@gmail.com>
2026-01-24 23:12:22 +0100

    Fix spotify

--- system/modules/defaultPackages.nix
+++ system/modules/defaultPackages.nix
@@ -22,5 +22,20 @@
     gimp3-with-plugins
     bibata-cursors
     spotify
+    # These fixes dont quite work yet. Use ctrl - while in spotify to scale down.
+    (pkgs.symlinkJoin {
+      name = "spotify";
+      paths = [ pkgs.spotify ];
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/spotify \
+          --set GDK_SCALE 1 \
+          --set GDK_DPI_SCALE 1 \
+          --add-flags "--disable-gpu" \
+          --add-flags "--enable-features=UseOzonePlatform" \
+          --add-flags "--ozone-platform=wayland" \
+          --add-flags "--force-device-scale-factor=1"
+      '';
+    })
   ];
 }
