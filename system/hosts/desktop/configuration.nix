7e634d0aee7e54a84d57f3d7beda639f696c77be
nainteeth <knusperpommes@gmail.com>
2026-03-02 22:14:09 +0100

    cleanup configuration.nix and update

--- system/hosts/desktop/configuration.nix
+++ system/hosts/desktop/configuration.nix
@@ -1,14 +1,12 @@
 {
-  config,
   pkgs,
   username,
   ...
 }:
-
 {
   imports = [
     ./hardware-configuration.nix
-    ../../modules/doom.nix
+    # ../../modules/doom.nix
     ../../modules/defaultPackages.nix
     ../../modules/hyprland.nix
   ];
@@ -20,108 +18,105 @@
     input-remapper
     polkit_gnome
   ];
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+  programs = {
+    steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraProfile = ''
+          export GDK_SCALE=1
+          export GDK_DPI_SCALE=1
+          export QT_AUTO_SCREEN_SCALE_FACTOR=0
+          export QT_SCALE_FACTOR=1
+        '';
+        extraArgs = "-forcedesktopscaling 1.0 -no-force-device-scale-factor";
+      };
+    };
+    ssh.startAgent = true;
   };
 
-  programs.steam = {
-    enable = true;
-    package = pkgs.steam.override {
-      extraProfile = ''
-        export GDK_SCALE=1
-        export GDK_DPI_SCALE=1
-        export QT_AUTO_SCREEN_SCALE_FACTOR=0
-        export QT_SCALE_FACTOR=1
-      '';
-      extraArgs = "-forcedesktopscaling 1.0 -no-force-device-scale-factor";
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    openrazer.enable = true;
+    openrazer.users = [ "nainteeth" ];
+    opentabletdriver.enable = true;
+    wooting.enable = true;
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
+    enableAllFirmware = true;
+  };
+
+  services = {
+    flatpak.enable = true;
+    dbus.enable = true;
+    input-remapper.enable = true;
+    udev.extraRules = ''
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", MODE="0666", GROUP="input"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="31e3", MODE="0666", GROUP="input"
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", MODE="0666", GROUP="input"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="3434", MODE="0666", GROUP="input"
+    '';
+    cloudflare-warp.enable = true;
+    mullvad-vpn.enable = true;
+    displayManager.ly = {
+      enable = true;
+    };
+    # This is needed for UxPlay to work
+    avahi = {
+      enable = true;
+      nssmdns4 = true; # Enable mDNS resolution for NSS
+      openFirewall = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        domain = true;
+        hinfo = true;
+        userServices = true;
+        workstation = true;
+      };
+    };
+    pipewire = {
+      enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+    printing.enable = true;
+  };
+
+  security = {
+    polkit.enable = true;
+    rtkit.enable = true;
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
+    ];
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        7000
+        7001
+        7100
+      ];
+      allowedUDPPorts = [
+        5353
+        6000
+        6001
+        7011
+      ];
     };
   };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  security.polkit.enable = true;
-  services.dbus.enable = true;
-
-  # Razer mouse
-  hardware.openrazer.enable = true;
-  hardware.openrazer.users = [ "nainteeth" ];
-  services.input-remapper.enable = true;
-
-  # OSU! starter pack
-  hardware.opentabletdriver.enable = true;
-  hardware.wooting.enable = true;
-  services.udev.extraRules = ''
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", MODE="0666", GROUP="input"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="31e3", MODE="0666", GROUP="input"
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", MODE="0666", GROUP="input"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="3434", MODE="0666", GROUP="input"
-  '';
-
-  networking.networkmanager.enable = true;
-
-  # Trying to play hypixel skyblock in eu
-  services.cloudflare-warp.enable = true;
-  networking = {
-    nameservers = [
-      "1.1.1.1"
-      "8.8.8.8"
-    ];
-  };
-  services.mullvad-vpn.enable = true;
-
-  services.displayManager.ly = {
-    enable = true;
-  };
-
-  # This is needed for UxPlay to work
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true; # Enable mDNS resolution for NSS
-    openFirewall = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      domain = true;
-      hinfo = true;
-      userServices = true;
-      workstation = true;
-    };
-  };
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [
-      7000
-      7001
-      7100
-    ];
-    allowedUDPPorts = [
-      5353
-      6000
-      6001
-      7011
-    ];
-  };
-
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "de_DE.UTF-8";
-
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  services.printing.enable = true;
-
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
-  hardware.enableAllFirmware = true;
 
   users.users.${username} = {
     isNormalUser = true;
@@ -135,10 +130,6 @@
     ];
     shell = pkgs.bash;
   };
-
-  services.flatpak.enable = true;
-
-  programs.ssh.startAgent = true;
 
   systemd.user.services.polkit-gnome-authentication-agent-1 = {
     description = "polkit-gnome-authentication-agent-1";
@@ -161,9 +152,7 @@
     ];
     warn-dirty = false;
   };
-
   nixpkgs.config.allowUnfree = true;
-
   console.keyMap = "de";
   system.stateVersion = "25.05";
 }
--- system/hosts/laptop/configuration.nix
+++ system/hosts/laptop/configuration.nix
@@ -1,9 +1,13 @@
-{ config, pkgs, username, ... }:
+{
+  pkgs,
+  username,
+  ...
+}:
 
 {
   imports = [
     ./hardware-configuration.nix
-    ../../modules/doom.nix
+    # ../../modules/doom.nix
     ../../modules/defaultPackages.nix
     ../../modules/hyprland.nix
   ];
@@ -12,42 +16,53 @@
   environment.systemPackages = with pkgs; [
 
   ];
+  programs = {
+    ssh.startAgent = true;
+  };
+
+  hardware = {
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
+    enableAllFirmware = true;
+  };
+
+  services = {
+    flatpak.enable = true;
+    printing.enable = true;
+    displayManager.ly = {
+      enable = true;
+    };
+    pipewire = {
+      enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      };
+    };
+    libinput.enable = true;
+  };
+
+  security = {
+    rtkit.enable = true;
+  };
+
+  networking = {
+    networkmanager.enable = true;
+  };
 
   boot.loader.grub = {
     enable = true;
     device = "/dev/sda";
   };
 
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    };
-  };
-  services.libinput.enable = true;
-  networking.networkmanager.enable = true;
-  services.displayManager.ly = {
-    enable = true;
-  };
-
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "de_DE.UTF-8";
-
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  programs.ssh.startAgent = true;
-  services.flatpak.enable = true;
-  services.printing.enable = true;
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  hardware.enableAllFirmware = true;
 
   users.users.${username} = {
     isNormalUser = true;
@@ -58,16 +73,17 @@
       "video"
       "audio"
     ];
-  shell = pkgs.bash;
+    shell = pkgs.bash;
   };
 
   nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
     warn-dirty = false;
   };
-
   nixpkgs.config.allowUnfree = true;
-
   console.keyMap = "de";
   system.stateVersion = "25.05";
 }
