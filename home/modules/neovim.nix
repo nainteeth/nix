4824f28ef42696633cdc5417a5bb1763e3b64bb3
nainteeth <knusperpommes@gmail.com>
2026-03-05 22:51:36 +0100

    nvim fonts

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -3,4 +3,20 @@
     enable = true;
     defaultEditor = true;
   };
+  home.packages = with pkgs; [
+    ripgrep
+    fd
+    fzf
+    lazygit
+
+    gcc
+    tree-sitter
+
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.hack
+    nerd-fonts.meslo-lg
+    nerd-fonts.symbols-only
+    nerd-fonts.ubuntu-mono
+  ];
 }
